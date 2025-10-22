7354df784747d864d27055c9e2a668427b861bc7
sylvain senechal <senechalsylvain.dev@gmail.com>
2025-10-22 17:12:43 +0200

    added test for error handler
    ISSUE: CLDSRVCLT-1

---
+++ tests/testErrorHandling.test.ts
@@ -0,0 +1,53 @@
+import { 
+    CloudserverClient,
+    MultipleBackendDeleteObjectInput,
+    MultipleBackendDeleteObjectCommand,
+    GetObjectInput,
+    GetObjectCommand,
+} from '@scality/cloudserverclient';
+import { createTestClient, testConfig } from './testSetup';
+
+describe('CloudServer test error handling', () => {
+    let client: CloudserverClient;
+
+    beforeAll(() => {
+        client = createTestClient();
+    });
+
+    it.skip('should test xml parsing', async () => {
+        // Run CloudServer with : S3VAULT=mem S3METADATA=mem S3DATA=mem REMOTE_MANAGEMENT_DISABLE=true yarn start
+        // Needs to tamper with Cloudserver response manually to return an html error..
+        try {
+            const getInput: GetObjectInput = {
+                Bucket: testConfig.bucketName,
+                Key: 'notAKey',
+            };
+            const getCommand = new GetObjectCommand(getInput);
+            const getData = await client.send(getCommand);
+        } catch (err: any) {
+            console.log('Error:', err);
+            console.log('Error name:', err.name);
+            console.log('Error message:', err.message);
+            console.log('Error code:', err.$metadata?.httpStatusCode);
+        }
+    });
+
+    it('should test html parsing', async () => {
+        // Run CloudServer with : S3VAULT=mem S3METADATA=mem S3DATA=mem REMOTE_MANAGEMENT_DISABLE=true yarn start
+        try {
+            const deleteInput: MultipleBackendDeleteObjectInput = {
+                Bucket: 'testConfig.bucketName',
+                Key: 'notAKey',
+                StorageClass: 'us-east-1',
+                StorageType: 'file'
+            };
+            const commandDelete = new MultipleBackendDeleteObjectCommand(deleteInput);
+            const deleteResult = await client.send(commandDelete);
+        } catch (err: any) {
+            console.log('Error:', err);
+            console.log('Error name:', err.name);
+            console.log('Error message:', err.message);
+            console.log('Error code:', err.$metadata?.httpStatusCode);
+        }
+    });
+});
