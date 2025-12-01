101daf6975f1f2ad281637d07be04e5829fd93b1
sylvain senechal <senechalsylvain.dev@gmail.com>
2025-12-01 14:52:26 +0100

    added release workflow
    ISSUE: CLDSRVCLT-7

--- build/smithy/source/typescript-codegen/dist-types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from "./CloudserverClient";
-export * from "./Cloudserver";
-export { ClientInputEndpointParameters } from "./endpoint/EndpointParameters";
-export type { RuntimeExtension } from "./runtimeExtensions";
-export type { CloudserverExtensionConfiguration } from "./extensionConfiguration";
-export * from "./commands";
-export * from "./models";
-export { CloudserverServiceException } from "./models/CloudserverServiceException";
