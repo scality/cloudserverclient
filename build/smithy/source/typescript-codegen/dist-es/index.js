ee1ad3a137d207c0a92a30323f83b9d4f6f8db12
sylvain senechal <senechalsylvain.dev@gmail.com>
2025-12-01 14:52:26 +0100

    added release workflow
    ISSUE: CLDSRVCLT-7

--- build/smithy/source/typescript-codegen/dist-es/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./CloudserverClient";
-export * from "./Cloudserver";
-export * from "./commands";
-export * from "./models";
-export { CloudserverServiceException } from "./models/CloudserverServiceException";
