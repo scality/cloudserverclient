1e7604267a1163ea0771296d36962b8dc8f60814
sylvain senechal <senechalsylvain.dev@gmail.com>
2025-12-10 15:57:09 +0100

    update gitignore to ignore build following new release workflow
    ISSUE: CLDSRVCLT-7

--- build/smithy/source/typescript-codegen/dist-types/endpoint/endpointResolver.d.ts
+++
@@ -1,5 +0,0 @@
-import { EndpointParameters } from "./EndpointParameters";
-import { EndpointV2, Logger } from "@smithy/types";
-export declare const defaultEndpointResolver: (endpointParams: EndpointParameters, context?: {
-    logger?: Logger;
-}) => EndpointV2;
--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { CloudserverClient as GeneratedCloudserverClient, CloudserverClientConfig } from '../build/smithy/source/typescript-codegen';
-export * from '../build/smithy/source/typescript-codegen';
-export * from './utils';
-export declare class CloudserverClient extends GeneratedCloudserverClient {
-    constructor(config: CloudserverClientConfig);
-}
