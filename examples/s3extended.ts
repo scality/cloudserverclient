3840086271c59dd64a5480c65b65649313d96ec6
sylvain senechal <senechalsylvain.dev@gmail.com>
2026-01-14 11:36:32 +0100

    added s3extended example
    ISSUE: CLDSRVCLT-6

---
+++ examples/s3extended.ts
@@ -0,0 +1,21 @@
+
+import { S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
+import { ListObjectsV2ExtendedCommand } from '@scality/cloudserverclient/clients/s3Extended';
+
+const config: S3ClientConfig = {
+    endpoint: 'http://localhost:8000',
+    credentials: {
+        accessKeyId: 'accessKey1',
+        secretAccessKey: 'verySecretKey1',
+    },
+    region: 'us-east-1',
+};
+
+const client = new S3Client(config);
+
+const response = await client.send(
+    new ListObjectsV2ExtendedCommand({
+        Bucket: 'aBucketName',
+        Query: 'content-length > 0',
+    }),
+);
