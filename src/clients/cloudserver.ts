2744e09b7f8326b6e282e0b5208ebc21062adf41
sylvain senechal <senechalsylvain.dev@gmail.com>
2026-02-10 10:43:23 +0100

    add proxyBackbeatApis to the general cloudserver client
    Issue: CLDSRVCLT-9

--- src/clients/cloudserver.ts
+++ src/clients/cloudserver.ts
@@ -1,5 +1,6 @@
 import { BackbeatRoutesClient } from './backbeatRoutes';
 import { BucketQuotaClient } from './bucketQuota';
+import { ProxyBackbeatApisClient } from './proxyBackbeatApis';
 import { CloudserverBackbeatRoutesClientConfig } from '../../build/smithy/cloudserverBackbeatRoutes/typescript-codegen';
 
 export type CloudserverClientConfig = CloudserverBackbeatRoutesClientConfig;
@@ -7,9 +8,11 @@ export type CloudserverClientConfig = CloudserverBackbeatRoutesClientConfig;
 export class CloudserverClient {
     public readonly backbeatRoutes: BackbeatRoutesClient;
     public readonly bucketQuota: BucketQuotaClient;
+    public readonly proxyBackbeatApis: ProxyBackbeatApisClient;
 
     constructor(config: CloudserverClientConfig) {
         this.backbeatRoutes = new BackbeatRoutesClient(config);
         this.bucketQuota = new BucketQuotaClient(config);
+        this.proxyBackbeatApis = new ProxyBackbeatApisClient(config);
     }
 }
